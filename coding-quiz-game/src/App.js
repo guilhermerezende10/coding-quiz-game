d010a957f05daa62c7b3c56f512f03c2075870fb
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-08 08:00:02 -0300

    initial commit

---
+++ coding-quiz-game/src/App.js
@@ -0,0 +1,21 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Coding Quiz Game</h1>
+      <select>
+        <option value="Linux">Linux</option>
+        <option value="DevOps">DevOps</option>
+        <option value="Networking">Networking</option>
+        <option value="Cloud">Cloud</option>
+        <option value="PHP">PHP</option>
+        <option value="JS">JS</option>
+        <option value="Python ">Python</option>
+        <option value="React ">React</option>
+      </select>
+    </div>
+  );
+}
+
+
