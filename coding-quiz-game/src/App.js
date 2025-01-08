25e43236bbd9df8d4045f54b4342531d57bed781
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-08 10:04:00 -0300

    first structure

--- coding-quiz-game/src/App.js
+++ coding-quiz-game/src/App.js
@@ -1,21 +1,77 @@
-import './styles.css';
+import { useState } from "react";
+import "./styles.css";
+
+const API_KEY = "2Um4w0zjk1qtARGXcoFU6zNqliZrfkGPECVjHmjc";
+
+const categories = [
+  "React",
+  "JS",
+  "Python",
+  "PHP",
+  "Cloud",
+  "Networking",
+  "DevOps",
+  "Linux",
+];
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Coding Quiz Game</h1>
-      <select>
-        <option value="Linux">Linux</option>
-        <option value="DevOps">DevOps</option>
-        <option value="Networking">Networking</option>
-        <option value="Cloud">Cloud</option>
-        <option value="PHP">PHP</option>
-        <option value="JS">JS</option>
-        <option value="Python ">Python</option>
-        <option value="React ">React</option>
-      </select>
+      <h1 className="title">Coding Quiz Game</h1>
+      <QueryOptions />
     </div>
   );
 }
 
+async function fetchQuiz(category) {
+  console.log(category);
+  const url = "https://quizapi.io/api/v1/questions";
+  const params = new URLSearchParams({
+    API_KEY,
+    limit: "10",
+    category: category,
+  });
 
+  try {
+    const data = await fetch(`${url}?${params.toString()}`, {
+      method: "GET",
+    });
+    const res = await data.json();
+    console.log(res);
+
+    if (!res.ok) throw new Error(`Something went wrong. Try again later.`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function QueryOptions() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  return (
+    <div className="quiz-categories">
+      <h2 className="subtitle">Choose your quiz's category</h2>
+      <select
+        className="list-categories"
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        {categories.map((category) => (
+          <Category category={category} key={category} />
+        ))}
+      </select>
+      <button
+        className="btn-categories"
+        onClick={() => fetchQuiz(selectedCategory)}
+      >
+        Choose
+      </button>
+    </div>
+  );
+}
+
+function Category({ category }) {
+  return (
+    <option value={category} className="category">
+      {category}
+    </option>
+  );
+}
