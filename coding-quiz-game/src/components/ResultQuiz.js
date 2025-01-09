f9cc75147a2f9dd063f97408fb03ca95adae56fe
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-09 13:23:13 -0300

    questionNumber prop to show X/X

--- coding-quiz-game/src/components/ResultQuiz.js
+++ coding-quiz-game/src/components/ResultQuiz.js
@@ -1,7 +1,8 @@
-export function ResultQuiz({ score, onPlayAgain }) {
+
+export function ResultQuiz({ score, onPlayAgain, questionNumber }) {
   return (
     <div className="result-box">
-      <h3 className="result-score">Your Score: {score}</h3>
+      <h3 className="result-score">Your Score: {score}/{questionNumber}</h3>
       <button className="result-btn" onClick={onPlayAgain}>
         Play Again
       </button>
